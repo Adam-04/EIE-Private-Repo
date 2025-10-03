b45f8cea748d4d2d7462ca9d6c93ba12bd1c2dc6
Adam-04 <144634921+Adam-04@users.noreply.github.com>
2025-10-02 20:14:30 -0600

    Flashing lights

--- app/src/main.c
+++ app/src/main.c
@@ -6,20 +6,32 @@
 #include <zephyr/drivers/gpio.h>
 
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 
 int main(void)
 {
     int ret;
-    if(!gpio_is_ready_dt(&led0)) {
+    int ret1;
+    int ret_2;
+    if(!gpio_is_ready_dt(&led0) || !gpio_is_ready_dt(&led1)) {
         return -1;
     }
     ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-    if(ret<0){
+    ret1 = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+    ret_2 = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+    if(ret<0 || ret1<0) {
         return -1;
     }
     while(1){
+        gpio_pin_toggle_dt(&led0);
+        gpio_pin_toggle_dt(&led2);
+        gpio_pin_toggle_dt(&led1);
+        k_msleep(1000);
     }
     return 0;
 }
